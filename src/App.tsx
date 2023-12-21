69ce0f37db7eaa31d09eaddb3ca800ae8b220b36
matiassemelmaning <MSemelman@ext.bancodelsol.com>
2023-12-20 23:47:52 -0300

    Add i18n support for localization

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { LeftView, RightView } from "./components";
 import { getCurrencyValues } from "./api/currencyService";
+import { useTranslation } from "react-i18next";
 
 function App() {
   const [amount, setAmount] = useState(0);
   const [values, setValues] = useState({});
+  const { t } = useTranslation();
 
   useEffect(() => {
     const fetchValues = async () => {
@@ -16,16 +18,16 @@ function App() {
 
   return (
     <div className="min-h-screen">
-      <h2 className="text-center py-20 text-2xl">
-        Argentina Currency Exchange
-      </h2>
+      <h2 className="text-center py-20 text-2xl">{t("title1")}</h2>
       <div className=" flex justify-center items-center">
         <div className="bg-white md:flex rounded-t-xl rounded-b-xl gap-1 shadow-lg">
           <LeftView setAmount={setAmount} amount={amount} />
           <RightView amount={amount} values={values} />
         </div>
       </div>
-      <h1 className="text-center pt-10 md:pt-20 text-xl">By Matias Semelman</h1>
+      <h1 className="text-center pt-10 md:pt-20 text-xl">
+        {t("by")} Matias Semelman
+      </h1>
     </div>
   );
 }
